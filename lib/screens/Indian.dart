736afb80d512b896786d538da35a4d24a3543a51
Sahil Agarwal <agarwalsahil0210@gmail.com>
2021-05-23 15:15:21 +0530

    minor changes

--- lib/screens/Indian.dart
+++ lib/screens/Indian.dart
@@ -26,7 +26,6 @@ class _Indian extends State<Indian> {
       "https://api.apify.com/v2/key-value-stores/toDWvRj1JpTXiM8FF/records/LATEST?disableRedirect=true";
   List data;
 
-
   static String active,
       totalRecovered,
       nRec,
@@ -36,12 +35,10 @@ class _Indian extends State<Indian> {
       nInfec,
       title;
 
-
   @override
   void initState() {
     super.initState();
     this.getJsonData();
-
   }
 
   // ignore: missing_return
@@ -59,142 +56,129 @@ class _Indian extends State<Indian> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-        appBar: AppBar(
+      appBar: AppBar(
         title: Text('Concure'),
-    ),
+      ),
 
-      body:data==null?Center(child: CircularProgressIndicator()): ListView.builder(
-          itemCount: data == null ? 0 : data.length,
-          itemBuilder: (BuildContext context, int index) {
-      title = data[index]['region'].toString();
-      active = data[index]['activeCases'].toString();
-      totalRecovered = data[index]['recovered'].toString();
-      nRec = data[index]['newRecovered'].toString();
+      body: data == null
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: data == null ? 0 : data.length,
+              itemBuilder: (BuildContext context, int index) {
+                title = data[index]['region'].toString();
+                active = data[index]['activeCases'].toString();
+                totalRecovered = data[index]['recovered'].toString();
+                nRec = data[index]['newRecovered'].toString();
 
-      tdecre = data[index]['deceased'].toString();
-      nDecre = data[index]['newDeceased'].toString();
-      tinfec = data[index]['totalInfected'].toString();
-      nInfec = data[index]['newInfected'].toString();
+                tdecre = data[index]['deceased'].toString();
+                nDecre = data[index]['newDeceased'].toString();
+                tinfec = data[index]['totalInfected'].toString();
+                nInfec = data[index]['newInfected'].toString();
 
+                return new Container(
+                  child: new Center(
+                    child: new Column(
+                        crossAxisAlignment: CrossAxisAlignment.stretch,
+                        children: <Widget>[
+                          new Card(
+                            child: ExpansionTile(
+                              title: Text(
+                                title,
+                                style: TextStyle(
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                              children: <Widget>[
+                                ListTile(
+                                  title: Text(
+                                    'Active Cases:  ' +
+                                        data[index]['activeCases'].toString(),
+                                    style: TextStyle(
+                                        fontSize: 16.0,
+                                        fontWeight: FontWeight.w700,
+                                        color: Colors.red),
+                                  ),
+                                ),
+                                Pan(
+                                  tr: totalRecovered,
+                                  nr: nRec,
+                                  td: tdecre,
+                                  nd: nDecre,
+                                  ti: tinfec,
+                                  ni: nInfec,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ]),
+                  ),
+                );
+              }),
 
+      //NAVIGATION BAR
 
-    return new Container(
-    child: new Center(
-    child: new Column(
-    crossAxisAlignment: CrossAxisAlignment.stretch,
-    children: <Widget>[
-    new Card(
-
-    child: ExpansionTile(
-    title: Text(
-    title,
-    style: TextStyle(
-
-    fontSize: 16.0, fontWeight: FontWeight.w500),
-    ),
-    children: <Widget>[
-    ListTile(
-    title: Text(
-        'Active Cases:  ' +
-        data[index]['activeCases'].toString(),
-      style: TextStyle(
-          fontSize: 16.0,
-          fontWeight: FontWeight.w700,
-          color: Colors.red),
-    ),),
-    Pan(
-
-    tr: totalRecovered,
-    nr: nRec,
-    td: tdecre,
-    nd: nDecre,
-    ti: tinfec,
-    ni: nInfec,
-
-    ),
-
-    ],
-    ),),
-    ]
-    ),
-    ),
-    );
-    }),
-
-
-
-
-
-
-
-
-
-
-
-    //NAVIGATION BAR
-
-    bottomNavigationBar: Container(
-    decoration: BoxDecoration(
-    color: Colors.white,
-    boxShadow: [
-    BoxShadow(
-    blurRadius: 20,
-    color: Colors.black.withOpacity(.1),
-    )
-    ],
-    ),
-    child: SafeArea(
-    child: Padding(
-    padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
-    child: GNav(
-    // rippleColor: Colors.black87,
-    // hoverColor: Colors.yellow,
-    gap: 8,
-    // activeColor: Colors.black,
-    iconSize: 24,
-    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-    duration: Duration(milliseconds: 400),
-    // tabBackgroundColor: Colors.yellow,
-    color: Colors.black,
-    tabs: [
-    GButton(
-    icon: Icons.apps,
-    iconSize: 30,
-    text: 'Home',
-    backgroundColor: Colors.red[100],
-    textColor: Colors.red,
-    iconActiveColor: Colors.red,
-    iconColor: Colors.red,
-    onPressed: () {
-    Navigator.pushReplacement(
-    context,
-    MaterialPageRoute(
-    builder: (context) => DashboardScreen(),
-    ),
-    );
-    },
-    ),
-    GButton(
-    icon: Icons.find_in_page,
-    iconColor: Colors.purpleAccent,
-    text: 'Countries',
-    backgroundColor: Colors.purple[100],
-    textColor: Colors.purple,
-    iconActiveColor: Colors.purpleAccent[200],
-    onPressed: () {
-    Navigator.pushReplacement(
-    context,
-    MaterialPageRoute(builder: (context) => Cont()),
-    );
-    },
-    ),
-    GButton(
-    icon: Icons.countertops,
-    text: 'States',
-    iconColor: Colors.pink,
-    backgroundColor: Colors.pink[100],
-    textColor: Colors.pink,
-    iconActiveColor: Colors.redAccent,
+      bottomNavigationBar: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 20,
+              color: Colors.black.withOpacity(.1),
+            )
+          ],
+        ),
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+            child: GNav(
+              // rippleColor: Colors.black87,
+              // hoverColor: Colors.yellow,
+              gap: 8,
+              // activeColor: Colors.black,
+              iconSize: 24,
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+              duration: Duration(milliseconds: 400),
+              // tabBackgroundColor: Colors.yellow,
+              color: Colors.black,
+              tabs: [
+                GButton(
+                  icon: Icons.apps,
+                  iconSize: 30,
+                  text: 'Home',
+                  backgroundColor: Colors.red[100],
+                  textColor: Colors.red,
+                  iconActiveColor: Colors.red,
+                  iconColor: Colors.red,
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DashboardScreen(),
+                      ),
+                    );
+                  },
+                ),
+                GButton(
+                  icon: Icons.find_in_page,
+                  iconColor: Colors.purpleAccent,
+                  text: 'Countries',
+                  backgroundColor: Colors.purple[100],
+                  textColor: Colors.purple,
+                  iconActiveColor: Colors.purpleAccent[200],
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => Cont()),
+                    );
+                  },
+                ),
+                GButton(
+                  icon: Icons.countertops,
+                  text: 'States',
+                  iconColor: Colors.pink,
+                  backgroundColor: Colors.pink[100],
+                  textColor: Colors.pink,
+                  iconActiveColor: Colors.redAccent,
 
 //               onPressed: () {
 //                 Navigator.push(context,
@@ -202,94 +186,82 @@ class _Indian extends State<Indian> {
 // //
 //
 //               },
+                ),
+                GButton(
+                  icon: Icons.settings,
+                  text: 'Settings',
+                  iconColor: Colors.blue,
+                  backgroundColor: Colors.blue[100],
+                  textColor: Colors.blue[500],
+                  iconActiveColor: Colors.blue[600],
+                  onPressed: () {
+                    //TODO
 
-    ),
-    GButton(
-    icon: Icons.settings,
-    text: 'Settings',
-    iconColor: Colors.blue,
-    backgroundColor: Colors.blue[100],
-    textColor: Colors.blue[500],
-    iconActiveColor: Colors.blue[600],
-    onPressed: () {
-
-    //TODO
-
-     Navigator.pushReplacement(
-       context,
-      MaterialPageRoute(builder: (context) => SettingPage()),
-     );
-
-    },
-    ),
-
-    ],
-    selectedIndex: 2,
-    ),
-    ),
-    ),
-    ),
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(builder: (context) => SettingPage()),
+                    );
+                  },
+                ),
+              ],
+              selectedIndex: 2,
+            ),
+          ),
+        ),
+      ),
     );
-    }
+  }
+}
 
-    }
-
-    Widget Pan(
-    {String tr, String nr, String td, String nd, String ti, String ni}) {
-    return Container(
+Widget Pan({String tr, String nr, String td, String nd, String ti, String ni}) {
+  return Container(
     // height: 0,
     child: GridView(
-    shrinkWrap: true,
-    physics: NeverScrollableScrollPhysics(),
-
-    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-    crossAxisCount: 2, childAspectRatio: 3),
-    children: <Widget>[
-    StatusPanel2(
-
-    textColor: Colors.green,
-    title: 'Total Recovered',
-    count: tr,
+      shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2, childAspectRatio: 3),
+      children: <Widget>[
+        StatusPanel2(
+          textColor: Colors.green,
+          title: 'Total Recovered',
+          count: tr,
+        ),
+        StatusPanel2(
+          textColor: Colors.green,
+          title: 'Newly Recovered',
+          count: nr,
+        ),
+        StatusPanel2(
+          // panelColor: Colors.blue[100],
+          textColor: Colors.grey[600],
+          title: 'Total Deceased',
+          count: nd,
+        ),
+        StatusPanel2(
+          textColor: Colors.grey[600],
+          title: 'Newly Deceased',
+          count: nd,
+        ),
+        StatusPanel2(
+          textColor: Colors.red,
+          title: 'Total Infected',
+          count: ti,
+        ),
+        StatusPanel2(
+          // panelColor: Colors.blue[100],
+          textColor: Colors.red,
+          title: 'Newly Infected',
+          count: ni,
+        ),
+      ],
     ),
-    StatusPanel2(
-
-    textColor: Colors.green,
-    title: 'Newly Recovered',
-    count: nr,
-    ),
-    StatusPanel2(
-    // panelColor: Colors.blue[100],
-    textColor: Colors.grey[600],
-    title: 'Total Deceased',
-    count: nd,
-    ),
-    StatusPanel2(
-
-    textColor: Colors.grey[600],
-    title: 'Newly Deceased',
-    count: nd,
-    ),
-    StatusPanel2(
-
-    textColor: Colors.red,
-    title: 'Total Infected',
-    count: ti,
-    ),
-    StatusPanel2(
-    // panelColor: Colors.blue[100],
-    textColor:Colors.red ,
-    title: 'Newly Infected',
-    count: ni,
-    ),
-    ],
-    ),
-    );
-    }
-
+  );
+}
 
 // static String active, totalRecovered, nRec, tdecre, nDecre, tinfec, nInfec,title;
 
-  class StatusPanel2 extends StatelessWidget {
+class StatusPanel2 extends StatelessWidget {
   // final Color panelColor;
 
   final Color textColor;
@@ -298,32 +270,31 @@ class _Indian extends State<Indian> {
 
   final String count;
 
-  const StatusPanel2(
-  {Key key, this.textColor, this.title, this.count})
+  const StatusPanel2({Key key, this.textColor, this.title, this.count})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-  // double width = MediaQuery.of(context).size.width;
-  return Container(
-  // margin: EdgeInsets.all(5),
-  // color: panelColor,
-  // height: 40,
-  // width: width / 2,
-  child: Column(
-  mainAxisAlignment: MainAxisAlignment.center,
-  children: <Widget>[
-  Text(title,
-  style: TextStyle(
-  fontWeight: FontWeight.bold, fontSize: 15, color: textColor)),
-  SizedBox(height: 4,),
-  Text(count,
-  style: TextStyle(
-  fontWeight: FontWeight.bold, fontSize: 15, color: textColor))
-  ],
-  ),
-  );
+    // double width = MediaQuery.of(context).size.width;
+    return Container(
+      // margin: EdgeInsets.all(5),
+      // color: panelColor,
+      // height: 40,
+      // width: width / 2,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(title,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold, fontSize: 15, color: textColor)),
+          SizedBox(
+            height: 4,
+          ),
+          Text(count,
+              style: TextStyle(
+                  fontWeight: FontWeight.bold, fontSize: 15, color: textColor))
+        ],
+      ),
+    );
   }
-  }
-
-
+}
--- lib/screens/SettingPage.dart
+++ lib/screens/SettingPage.dart
@@ -1,6 +1,5 @@
 import 'dart:async';
 
-
 import 'package:covid19_tracker/model/config.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -12,179 +11,170 @@ import 'Indian.dart';
 import 'dashboard.dart';
 
 class SettingPage extends StatefulWidget {
-  _SettingPage createState()=>_SettingPage();
+  _SettingPage createState() => _SettingPage();
 }
 
 class _SettingPage extends State<SettingPage> {
-     var isSwitched=false;
-    Future<bool>Savesettings(bool swit) async {
+  var isSwitched = false;
+  Future<bool> Savesettings(bool swit) async {
     // TODO: implement initState
 
-      SharedPreferences prefs = await SharedPreferences.getInstance();
-       prefs.setBool('isSwitched', isSwitched);
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('isSwitched', isSwitched);
 
-return prefs.commit();
-    }
+    return prefs.commit();
+  }
 
-     Future<bool> Getsettings() async{
-       SharedPreferences prefs = await SharedPreferences.getInstance();
-       isSwitched=prefs.getBool('isSwitched');
-       return isSwitched;
-     }
-@override
+  Future<bool> Getsettings() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    isSwitched = prefs.getBool('isSwitched');
+    return isSwitched;
+  }
+
+  @override
   void initState() {
     // TODO: implement initState
     super.initState();
 
-
-   Getsettings().then(update);
+    Getsettings().then(update);
   }
-     FutureOr update(bool value) {
-       setState(() {
-         isSwitched=value;
-       });
-     }
+
+  FutureOr update(bool value) {
+    setState(() {
+      isSwitched = !value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-
-   return Scaffold(
-        appBar: AppBar(
-             title: Text('Concure'),
-        ),
-     body:Container(
-      child: Column(
-           children: [
-                Container(
-                     child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                               Text("Set Dark Mode",style: TextStyle(fontSize: 20),),
-                               Switch(
-                                    value: isSwitched,
-                                    onChanged: (value){
-
-
-                                           isSwitched=value;
-                                           currentTheme.switchTheme();
-
-                                          Savesettings(isSwitched);
-
-
-                                    },
-                                    activeColor: Colors.orange,
-                                    activeTrackColor: Colors.orangeAccent,
-
-                               )
-                          ],
-                     ),
-                )
-           ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Concure'),
       ),
-     ),
+      body: Container(
+        child: Column(
+          children: [
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Set Dark Mode",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  Switch(
+                    value: isSwitched,
+                    onChanged: (value) {
+                      isSwitched = value;
+                      currentTheme.switchTheme();
 
-
-
-        bottomNavigationBar:  Container(
+                      Savesettings(isSwitched);
+                    },
+                    activeColor: Colors.orange,
+                    activeTrackColor: Colors.orangeAccent,
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+      bottomNavigationBar: Container(
         decoration: BoxDecoration(
-        color: Colors.white,
-        boxShadow: [
-        BoxShadow(
-        blurRadius: 20,
-        color: Colors.black.withOpacity(.1),
-        )
-        ],
+          color: Colors.white,
+          boxShadow: [
+            BoxShadow(
+              blurRadius: 20,
+              color: Colors.black.withOpacity(.1),
+            )
+          ],
         ),
         child: SafeArea(
-        child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
-        child: GNav(
-        // rippleColor: Colors.black87,
-        // hoverColor: Colors.yellow,
-        gap: 8,
-        // activeColor: Colors.black,
-        iconSize: 24,
-        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
-        duration: Duration(milliseconds: 400),
-        // tabBackgroundColor: Colors.yellow,
-        color: Colors.black,
-        tabs: [
-        GButton(
-        icon: Icons.apps,
-        iconSize: 30,
-        text: 'Home',
-        backgroundColor: Colors.red[100],
-        textColor: Colors.red,
-        iconActiveColor: Colors.red,
-        iconColor: Colors.red,
-        onPressed: () {
-        Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => DashboardScreen(),),);
-        },
-        ),
-        GButton(
-        icon: Icons.find_in_page,
-        iconColor: Colors.purpleAccent,
-        text: 'Countries',
-        backgroundColor: Colors.purple[100],
-        textColor: Colors.purple,
-        iconActiveColor: Colors.purpleAccent[200],
-         onPressed: () {
-          Navigator.push(
-             context,
-            MaterialPageRoute(builder: (context) => Cont()),
-           );}
-
-        ),
-        GButton(
-        icon: Icons.countertops,
-        text: 'States',
-        iconColor: Colors.pink,
-        backgroundColor: Colors.pink[100],
-        textColor: Colors.pink,
-        iconActiveColor: Colors.redAccent,
-        onPressed: () {
-        Navigator.pushReplacement(context,
-        MaterialPageRoute(builder: (context) => Indian()));
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 8),
+            child: GNav(
+              // rippleColor: Colors.black87,
+              // hoverColor: Colors.yellow,
+              gap: 8,
+              // activeColor: Colors.black,
+              iconSize: 24,
+              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 12),
+              duration: Duration(milliseconds: 400),
+              // tabBackgroundColor: Colors.yellow,
+              color: Colors.black,
+              tabs: [
+                GButton(
+                  icon: Icons.apps,
+                  iconSize: 30,
+                  text: 'Home',
+                  backgroundColor: Colors.red[100],
+                  textColor: Colors.red,
+                  iconActiveColor: Colors.red,
+                  iconColor: Colors.red,
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => DashboardScreen(),
+                      ),
+                    );
+                  },
+                ),
+                GButton(
+                    icon: Icons.find_in_page,
+                    iconColor: Colors.purpleAccent,
+                    text: 'Countries',
+                    backgroundColor: Colors.purple[100],
+                    textColor: Colors.purple,
+                    iconActiveColor: Colors.purpleAccent[200],
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => Cont()),
+                      );
+                    }),
+                GButton(
+                  icon: Icons.countertops,
+                  text: 'States',
+                  iconColor: Colors.pink,
+                  backgroundColor: Colors.pink[100],
+                  textColor: Colors.pink,
+                  iconActiveColor: Colors.redAccent,
+                  onPressed: () {
+                    Navigator.pushReplacement(context,
+                        MaterialPageRoute(builder: (context) => Indian()));
 //
-        // Navigator.pushReplacement(
-        //   context,
-        //   MaterialPageRoute(builder: (context) => MytestApp()),
-        // );
-        },
+                    // Navigator.pushReplacement(
+                    //   context,
+                    //   MaterialPageRoute(builder: (context) => MytestApp()),
+                    // );
+                  },
+                ),
+                GButton(
+                  icon: Icons.settings,
+                  text: 'Settings',
+                  iconColor: Colors.blue,
+                  backgroundColor: Colors.blue[100],
+                  textColor: Colors.blue[500],
+                  iconActiveColor: Colors.blue[600],
+                  onPressed: () {
+                    // Navigator.pushReplacement(
+                    //   context,
+                    //   MaterialPageRoute(builder: (context) => MytestApp()),
+                    // );
+                  },
+                ),
+              ],
+              selectedIndex: 3,
+              // onTabChange: (index) {
+              //   setState(() {
+              //     _selectedIndex = index;
+              //   });
+              // },
+            ),
+          ),
         ),
-        GButton(
-        icon: Icons.settings,
-        text: 'Settings',
-        iconColor: Colors.blue,
-        backgroundColor: Colors.blue[100],
-        textColor: Colors.blue[500],
-        iconActiveColor: Colors.blue[600],
-        onPressed: () {
-        // Navigator.pushReplacement(
-        //   context,
-        //   MaterialPageRoute(builder: (context) => MytestApp()),
-        // );
-        },
-        ),
-        ],
-        selectedIndex: 3,
-        // onTabChange: (index) {
-        //   setState(() {
-        //     _selectedIndex = index;
-        //   });
-        // },
-        ),
-        ),
-        ),
-        ),
-          );
-
-
-
-
-
+      ),
+    );
   }
-
-
-
-
 }
